ef5c5c22cb43b8cd1cbe030e29aaadd4daac6c0c
Glenon Mateus <glenonmateus@gmail.com>
2023-08-22 11:03:25 -0300

    comment

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 return {
     polish = function()
         local function yaml_ft(path, bufnr)
-            -- get content of buffer as string
-            local content = vim.filetype.getlines(bufnr)
-            if type(content) == "table" then
-                content = table.concat(content, "\n")
-            end
-            -- check if file is in roles, tasks, or handlers folder
-            local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-            if path_regex and path_regex:match_str(path) then
-                return "yaml.ansible"
-            end
-            -- check for known ansible playbook text and if found, return yaml.ansible
-            local regex = vim.regex "hosts:\\|tasks:"
-            if regex and regex:match_str(content) then
-                return "yaml.ansible"
-            end
+            -- -- get content of buffer as string
+            -- local content = vim.filetype.getlines(bufnr)
+            -- if type(content) == "table" then
+            --     content = table.concat(content, "\n")
+            -- end
+            -- -- check if file is in roles, tasks, or handlers folder
+            -- local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+            -- if path_regex and path_regex:match_str(path) then
+            --     return "yaml.ansible"
+            -- end
+            -- -- check for known ansible playbook text and if found, return yaml.ansible
+            -- local regex = vim.regex "hosts:\\|tasks:"
+            -- if regex and regex:match_str(content) then
+            --     return "yaml.ansible"
+            -- end
             -- ansible-lint
             local ansible_lint = io.open(".ansible-lint")
             if ansible_lint ~= nil then
